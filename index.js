6a55ee869c1dfb480a5d94ed9c9575c41158cdb4
zhasan1 <zubairhasan90@gmail.com>
2024-07-11 09:50:21 -0600

    updated index.js

--- index.js
+++ index.js
@@ -220,7 +220,7 @@ app.post("/users",
       return res.status(422).json({ errors: errors.array() });
     }
 
-    let hashedPassword = Users.hashedPassword(req.body.Password);
+    let hashedPassword = Users.hashPassword(req.body.Password);
 
     await Users.findOne({ Username: req.params.Username })
       .then((user) => {
